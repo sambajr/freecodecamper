8838e82df56097d42f8a095badbb1b076f743c0f
sambajr <metasamba.ng007@gmail.com>
2024-01-16 15:40:22 +0100

    Outside of the if statement, added number to sum_of_even_digits. Also, removed the print call.

--- Python/luhn_algo.py
+++ Python/luhn_algo.py
@@ -1,17 +1,20 @@
 #!/usr/bin/python3
-def verify_card_number(card_number):
+ef verify_card_number(card_number):
     sum_of_odd_digits = 0
     card_number_reversed = card_number[::-1]
     odd_digits = card_number_reversed[::2]
 
     for digit in odd_digits:
         sum_of_odd_digits += int(digit)
-    print(sum_of_odd_digits)
 
     sum_of_even_digits = 0
     even_digits = card_number_reversed[1::2]
     for digit in even_digits:
-        print(digit)
+        number = int(digit) * 2
+        if number >= 10:
+            number = (number // 10) + (number % 10)
+            print(number)
+
 def main():
     card_number = '4111-1111-4555-1142'
     card_translation = str.maketrans({'-': '', ' ': ''})
